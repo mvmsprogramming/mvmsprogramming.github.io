3e2bad0b83dca1b7fbea1e0b17482e103f4d53ad
Evan Thurston <evanthurston@gmail.com>
2015-09-06 22:34:49 -0700

    Heading is now red, fixed layout issue for mobile

--- js/app.js
+++ js/app.js
@@ -18,4 +18,12 @@ var app = angular.module('mvmsPcApp', ['ngMaterial'])
   .controller('pcController', function($scope, $http) {
     $http.get('https://www.googleapis.com/calendar/v3/calendars/3hjfsttq8ceskr9l5s8omgt2o0%40group.calendar.google.com/events/4n7sslhfu87jo4lm3mfa6d2jlo/instances?maxResults=1&timeMin=2015-09-06T15%3A30%3A00-07%3A00&fields=items(start%2Csummary)&key=AIzaSyCmqAoSy-W7dKQgDtmwLZW6l0kz0KPoJdw')
     .success(function(response){$scope.nextMeeting = response;});
+    // $scope.instructors = [
+    //   {
+    //     name: 'Ronak Shah';
+    //     title: 'dank memer';
+    //     description: 'd a n k m e m e s';
+    //     pic: 'res/images/ronak.png'
+    //   }
+    // ]
   });
--- js/controllers/MainController.js
+++
@@ -1,7 +0,0 @@
-// app.controller('MainController', ['$scope', function($scope, $http) {
-//   function fetch() {
-//     $http.get('https://www.googleapis.com/calendar/v3/calendars/3hjfsttq8ceskr9l5s8omgt2o0%40group.calendar.google.com/events/4n7sslhfu87jo4lm3mfa6d2jlo/instances?maxResults=1&timeMin=2015-09-06T15%3A30%3A00-07%3A00&fields=items(start%2Csummary)&key=AIzaSyCmqAoSy-W7dKQgDtmwLZW6l0kz0KPoJdw')
-//     .success(function(response){$scope.nextMeetDate = response;});
-//   }
-//   fetch();
-// }]);
