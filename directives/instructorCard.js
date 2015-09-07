a382d5bb040e1508dbcad39091251acaa9e2c5bb
Evan Thurston <evanthurston@gmail.com>
2015-09-07 01:53:49 -0700

    Revert "Last commit for the night. Added in functionality for instructors, added about section, and framework for contact and resources"
    This reverts commit 9610d40d55be37ace0b7468afe8c458d1e35bb04.

--- directives/instructorCard.js
+++
@@ -1,9 +0,0 @@
-app.directive('instructor', function() {
-	return {
-  	restrict: 'E',
-    scope: {
-      human: '='
-    },
-    templateUrl: './directives/instructorCard.html'
-  };
-});
--- js/app.js
+++ js/app.js
@@ -37,39 +37,13 @@ var app = angular.module('mvmsPcApp', ['ngMaterial'])
         return Math.round(difference_ms/one_day);
       }
       $scope.daysUntil = Date.daysBetween(currentDate, nextMeetDate);
-    })
-    $scope.instructors1 = [
-      {
-        name: 'Ronak Shah',
-        title: 'Head',
-        description: 'Currently a 10th grader at Westview High School, Ronak is the founder and head of MVMS Programming Club. Ronak is a passionate programmer who codes in Java, C++, HTML, CSS, and Javascript. You can contact Ronak at ronakshahultimate@gmail.com',
-        pic: 'res/images/horse_head.jpg'
-      },
-      {
-        name: 'Gunner Spencer',
-        title: 'Head',
-        description: 'Currently an 9th grader at Westview High School, Gunner is a seasoned programmer specializinng in HTML, CSS, and Javascript. You can contact Gunner at gunn686@gmail.com',
-        pic: 'res/images/horse_head.jpg'
-      },
-      {
-        name: 'Michael Lababera',
-        title: 'President',
-        description: 'Currently an 8th grader at Mesa Verde Middle School, Michael is a great programmer teaching mostly HTML and CSS. Along with being an amazing programmer, Michael is very mature and has great leadership, and consequently he is currently the head of management as well as being an instructor. You can contact Michael at Michael.jlabarbera@gmail.com',
-        pic: 'res/images/horse_head.jpg'
-      }
-    ]
-    $scope.instructors2 = [
-      {
-        name: 'Evan Thurston',
-        title: 'Instructor',
-        description: 'Currently an 8th grader at Mesa Verde Middle School, Evan is a fantastic programmer who mainly works in HTML, CSS, Javascript, Swift, and Unity C#. You can contact Evan at evanthurston@gmail.com',
-        pic: 'res/images/horse_head.jpg'
-      },
-      {
-        name: 'Assafi Cohen-Arazi',
-        title: 'Instructor',
-        description: "Currently an 8th grader at Mesa Verde Middle School, Assafi works hard at what he does. Boasting skills in Javascript, Assafi can write powerful programs in minutes. You can contact Assafi somewhere I don't know because he is a scrub",
-        pic: 'res/images/horse_head.jpg'
-      }
-    ];
+    });
+    // $scope.instructors = [
+    //   {
+    //     name: 'Ronak Shah';
+    //     title: 'dank memer';
+    //     description: 'd a n k m e m e s';
+    //     pic: 'res/images/ronak.png'
+    //   }
+    // ]
   });
