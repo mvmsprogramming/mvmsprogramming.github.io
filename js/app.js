f52e9383937b5336f1b6a2cde5debf4c80613c1a
JetpackCat39 <evanthurston@gmail.com>
2015-10-01 23:33:37 -0700

    reinstalling bower, date stuff, trying out directives

--- js/app.js
+++ js/app.js
@@ -1,43 +1,49 @@
-var app = angular.module('mvmsPcApp', ['ngMaterial'])
-    .config(function($mdThemingProvider, $mdIconProvider) {
-        $mdThemingProvider.theme('default')
-            .primaryPalette('teal', {
-                'default': '500',
-                'hue-1': '300',
-                'hue-2': '700',
-                'hue-3': 'A100'
-            })
-            .accentPalette('red', {
-                'default': '500',
-                'hue-1': '300',
-                'hue-2': '700',
-                'hue-3': 'A100'
-            });
-        $mdIconProvider
+var app = angular.module('mvmsPcApp', ['ngMaterial']);
+app.config(function($mdThemingProvider, $mdIconProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('teal', {
+            'default': '500',
+            'hue-1': '300',
+            'hue-2': '700',
+            'hue-3': 'A100'
+        })
+        .accentPalette('red', {
+            'default': '500',
+            'hue-1': '300',
+            'hue-2': '700',
+            'hue-3': 'A100'
+        });
+    $mdIconProvider
+})
+app.controller('pcController', function($scope, $http) {
+    var toRFC3339String = function(dateString) {
+        function pad(n) { return n < 10 ? '0' + n : n }
 
-    })
-    .controller('pcController', function($scope, $http) {
-        $http.get('https://www.googleapis.com/calendar/v3/calendars/3hjfsttq8ceskr9l5s8omgt2o0%40group.calendar.google.com/events/4n7sslhfu87jo4lm3mfa6d2jlo/instances?maxResults=1&fields=items(start)&singleEvents=true&key=AIzaSyCmqAoSy-W7dKQgDtmwLZW6l0kz0KPoJdw')
+        return dateString.getUTCFullYear() + '-'
+            + pad(dateString.getUTCMonth() + 1) + '-'
+            + pad(dateString.getUTCDate()) + 'T'
+            + pad(dateString.getUTCHours()) + ':'
+            + pad(dateString.getUTCMinutes()) + ':'
+            + pad(dateString.getUTCSeconds()) + 'Z'
+    }
+
+    var url = 'https://www.googleapis.com/calendar/v3/calendars/3hjfsttq8ceskr9l5s8omgt2o0%40group.calendar.google.com/events/4n7sslhfu87jo4lm3mfa6d2jlo/instances?maxResults=1&timeMin=' + toRFC3339String(new Date()) + '&fields=items(start)&singleEvents=true&key=AIzaSyCmqAoSy-W7dKQgDtmwLZW6l0kz0KPoJdw';
+
+    $http.get(url)
         .success(function(response){
-            $scope.nextMeeting = response.items[0].start.dateTime;
-            var nextMeetDateString = response.items[0].start.dateTime;
-            var yr = nextMeetDateString.substring(0, 4);
-            var mon = nextMeetDateString.substring(5, 7);
-            var mo = mon - 1;
-            var da = nextMeetDateString.substring(8, 10);
-            var mi = nextMeetDateString.substring(11, 13);
-            var se = nextMeetDateString.substring(14, 16);
-            var ms = nextMeetDateString.substring(17, 19);
-            var nextMeetDate = new Date(yr, mo, da, mi, se, ms);
-            var currentDate = new Date();
-            Date.daysBetween = function( date1, date2 ) {
-                var one_day=1000*60*60*24;
-                var date1_ms = date1.getTime();
-                var date2_ms = date2.getTime();
-                var difference_ms = date2_ms - date1_ms;
-                return Math.round(difference_ms/one_day);
-            }
-      $scope.daysUntil = Date.daysBetween(currentDate, nextMeetDate);
+
+        $scope.nextMeeting = new Date(response.items[0].start.dateTime);
+        var currentDate = new Date();
+
+        Date.daysBetween = function(date1, date2) {
+            var one_day=1000*60*60*24;
+            var date1_ms = date1.getTime();
+            var date2_ms = date2.getTime();
+            var difference_ms = date2_ms - date1_ms;
+            return Math.round(difference_ms/one_day);
+        }
+
+        $scope.daysUntil = Date.daysBetween(currentDate, $scope.nextMeeting);
     })
     $scope.instructors = [
       {
@@ -72,29 +78,35 @@ var app = angular.module('mvmsPcApp', ['ngMaterial'])
       }
     ]
     $scope.resources = [
-      {
-        name: 'Codecademy',
-        link: 'https://www.codecademy.com/learn',
-        description: "A wonderful learning site, Codecademy gives you all the resources you need to do some really cool things. If you want to start learning a new language, Codecademy is a great place to start if they have your language",
-        pic: 'res/images/codecademyLogo.png'
-      },
-      {
-        name: 'JsFiddle',
-        link: 'http://jsfiddle.net/',
-        description: "JsFiddle is an online IDE that supports HTML, CSS, Javascript, and many plugins for those languages. We often use JsFiddle in class because of it's simplicity",
-        pic: 'res/images/jsfiddleLogo.png'
-    },
-    {
-        name: 'Codepen',
-        link: 'http://codepen.io/',
-        description: 'Codepen is an online IDE that suppots HTML, CSS, Javascript, and many plugins for those languages. While we usually use JsFiddle in class, to share your code, you should use Codepen as the layout is simpler, at least that is what the general concensus is.',
-        pic: 'res/images/codepen.png'
-    },
-    {
-        name: 'Google',
-        link: 'https://www.google.com/',
-        description: 'Google is a great place to find answers to questions you may have. A fantastic application very few know of, usually if you type your problem into Google.com, you can find your answer. Magic!',
-        pic: 'res/images/google_logo.png'
-    }
+        {
+            name: 'Codecademy',
+            link: 'https://www.codecademy.com/learn',
+            description: "A wonderful learning site, Codecademy gives you all the resources you need to do some really cool things. If you want to start learning a new language, Codecademy is a great place to start if they have your language",
+            pic: 'res/images/codecademyLogo.png'
+        },
+        {
+            name: 'JsFiddle',
+            link: 'http://jsfiddle.net/',
+            description: "JsFiddle is an online IDE that supports HTML, CSS, Javascript, and many plugins for those languages. We often use JsFiddle in class because of it's simplicity",
+            pic: 'res/images/jsfiddleLogo.png'
+        },
+        {
+            name: 'Codepen',
+            link: 'http://codepen.io/',
+            description: 'Codepen is an online IDE that suppots HTML, CSS, Javascript, and many plugins for those languages. While we usually use JsFiddle in class, to share your code, you should use Codepen as the layout is simpler, at least that is what the general concensus is.',
+            pic: 'res/images/codepen.png'
+        },
+        {
+            name: 'Google',
+            link: 'https://www.google.com/',
+            description: 'Google is a great place to find answers to questions you may have. A fantastic application very few know of, usually if you type your problem into Google.com, you can find your answer. Magic!',
+            pic: 'res/images/google_logo.png'
+        }
     ];
-  });
+});
+app.directive('instructorCard', function() {
+    return {
+        restrict: 'EA',
+        templateUrl: 'directives/instructorCard.html'
+    }
+});
